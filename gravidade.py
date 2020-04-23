39100e42c6b6b7d0040b8078e8d81332b2c6a4a2
Lucas Assunção Costa <lucascosta28246@gmail.com>
2020-04-23 19:06:22 -0300

    Add files via upload

--- gravidade.py
+++ gravidade.py
@@ -1,27 +1,63 @@
-from graphics import *
+import pygame, math
+from pygame.locals import (
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+pygame.init()
+COMPRIMENTO_TELA = 1024
+ALTURA_TELA = 700
 
 class Planeta:
     def __init__(self, raio, massa, cor, pontoX, pontoY):
         self.raio = raio
         self.massa = massa
         self.cor = cor
-        self.corpo = Circle(Point(pontoX, pontoY), raio)
-        self.corpo.setFill(cor)
+        self.posX = pontoX
+        self.posY = pontoY
+
+    def Mover(self, modulo, angulo):
+        self.velX = modulo * math.sin(angulo)
+        self.velY = modulo * math.cos(angulo)
+        self.posX += self.velX
+        self.posY += self.velY        
+
+    def Desenhar(self, screen):
+        print(self.posX)
+        print(math.floor(self.posX))
+        pygame.draw.circle(screen, self.cor, (int(math.floor(self.posX)), int(math.floor(self.posY))), self.raio)
 
 class Sistema:
-    def __init__(self): # Inicializa os dados do sistema (constantes, tamanho da tela, etc)
-        self.win = GraphWin("Minha janela", 1000, 600)
-        self.terra = Planeta(30, 1, 'blue', 200, 200)
-        self.marte = Planeta(10, 1, 'red', 400, 350)
+    def __init__(self, planetas): # Inicializa os dados do sistema (constantes, tamanho da tela, etc)
+        self.win = pygame.display.set_mode([COMPRIMENTO_TELA, ALTURA_TELA])
+        self.planetas = planetas
 
     def loop(self):
-        self.terra.corpo.draw(self.win)
-        self.marte.corpo.draw(self.win)
-        self.win.getMouse()
-        self.win.close()
+        self.win.fill((0, 0, 0))
+        self.movimentaPlanetas()
+        pygame.display.update()
+        pygame.display.flip()
+
+    def movimentaPlanetas(self):
+        for planeta in self.planetas:
+            planeta.Mover(0.5, 80)
+            planeta.Desenhar(self.win)
+
+    #def calculaModulo(planeta):
+
+
+    def finaliza(self):
+        pygame.quit()
 
 def main():
-    solar = Sistema()
-    solar.loop()
+    terra = Planeta(20, 10, (0,0,255), 200, 350)
+    marte = Planeta(20, 10, (255,0,0), 800, 350)
+    rodando = True
+    solar = Sistema([terra, marte])
+    while rodando:
+        solar.loop()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                rodando = False
 
 main()
